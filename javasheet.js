ae74c0c2fcdcd045859fcc96e4ffe33e8a90495e
jfasoltholmes <jfasoltholmes@gmail.com>
2024-04-08 21:49:55 -0700

    Added black hair assignment

--- javasheet.js
+++ javasheet.js
@@ -5,3 +5,17 @@ function alertFunc() {
 function darkMode() {
     document.body.classList.toggle('dark-mode');
 }
+
+window.onload = function() {
+    const button = document.getElementById("myButton");
+    button.addEventListener("click", function() {
+        const liElements = document.getElementsByTagName("li");
+        for (let i = 0; i < liElements.length; i++) {
+            if (liElements[i].style.color === "magenta") {
+                liElements[i].style.color = "black";
+            } else {
+                liElements[i].style.color = "magenta";
+            }
+        }
+    });
+}
